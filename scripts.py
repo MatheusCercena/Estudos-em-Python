f5380f7732077926e4f3c981ea9bd52affaa1825
Matheus Cercena do Nascimento <certheus@protonmail.com>
2025-02-24 13:03:03 -0300

    operadores aritmeticos

--- scripts.py
+++ scripts.py
@@ -1,5 +1,36 @@
-print('Convertendo para inteiros ')
-n=int(input('Digite um numero'))
-print(n)
-print(type(n))
-#24/02 - aula 6 - https://youtu.be/hdDHg1p3YVc?list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6&t=1084
+# print('Convertendo para inteiros ')
+# n1=int(input('Digite um numero'))
+# n2=int(input('Digite outro numero'))
+# s=n1+n2
+
+# print(type(s))
+# print('A soma entre',n1,'e',n2,'vale',s)
+# print('ou')
+# print('A soma entre {} e {} vale {}'.format(n1, n2, s))#usar {} para substituir as variaveis
+# print('verificar se o valor pode ser convertido para numero')
+# n='5'
+# print(n.isnumeric())
+#
+# print('verificar se o valor é numerico')
+# sdgfsfgr='a'
+# print(sdgfsfgr.isalpha())
+#
+# print('verificar se o valor é alfanumerico')
+# dsamaskd='5a'
+# print(dsamaskd.isalnum())
+
+print('Operadores aritméticos')
+print('O que python tem de diferente é o uso de // para fazer uma divisão inteira, em que da o resultado da divisão sem o resto')
+print('A ordem de precedência é primeiro parenteses, depois potenciação, depois multiplicações, divisões, divisão inteira e resto')
+print('Outra forma de fazer potênciação, é usando pow(a, b), veja:')
+print('5 elevado a 3 é:', pow(5,3))
+print('O problema de usar pow(n1, n2) é que se perde a ordem de precedência.')
+print('Raiz quadrada')
+print('Pra calcular a raiz quadrada de um número, se faz o número elevado a 1/2. Se for raiz cúbica, se faz o número elevado a 1/3, veja:')
+print(int(15**(1/2)), 'raiz quadrada de 15 (aqui foi usado int para envelopar a conta para que o resultado de inteiro, sendo que ele sempre é arredondado para baixo.)')
+print(int(16**(1/2)),'raiz quadrada de 15, como numero real')
+print(16**(1/2),'raiz quadrada de 15, como numero real')
+print(125**(1/3), 'raiz cubica de 125')
+
+#24/02 manhã - aula 6 - https://youtu.be/hdDHg1p3YVc?list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6&t=108
+#24/02 meio-dia - aula 7 - https://youtu.be/Vw6gLypRKmY?list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6&t=1676
