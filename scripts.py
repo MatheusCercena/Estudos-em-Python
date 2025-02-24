00ff474ea57e6c6cbe6a92434b8f1b315b8b66e1
Matheus Cercena do Nascimento <certheus@protonmail.com>
2025-02-24 08:06:16 -0300

    vendo primeiros comandos

---
+++ scripts.py
@@ -0,0 +1,5 @@
+print('Convertendo para inteiros ')
+n=int(input('Digite um numero'))
+print(n)
+print(type(n))
+#24/02 - aula 6 - https://youtu.be/hdDHg1p3YVc?list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6&t=1084
